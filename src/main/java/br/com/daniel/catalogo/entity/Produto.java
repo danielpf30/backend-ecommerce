9ffa425f9f72d2251e76f746d5048b625b13c19c
danielpf30 <danielparnaiba1030@gmail.com>
2026-02-15 22:51:43 -0300

    Implementando a entity produto e configuraçoes

---
+++ src/main/java/br/com/daniel/catalogo/entity/Produto.java
@@ -0,0 +1,30 @@
+package br.com.daniel.catalogo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Nome é obrigatório!!")
+    @Column(nullable = false)
+    private String nome;
+
+    @Size(min = 10, message = "A descrição deve ter no minimo 10 caracteres!!")
+    private String descricao;
+
+    @NotBlank(message = "Informe qual a categoria!!")
+    @Size(min = 1, max = 20, message = "No minimo 1 e no maximo 20 caracteres!!")
+    private String categoria;
+
+    @NotNull
+    @PositiveOrZero(message = "Preço não pode ser negativo!!")
+    @Column(nullable = false)
+    private BigDecimal preco;
+}
